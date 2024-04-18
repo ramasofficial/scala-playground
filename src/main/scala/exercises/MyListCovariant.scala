e61db48d45217b9c94009739bb5c49d8ff8efdde
Ramas Grikštas <ramas.grikstas@kilo.health>
2024-04-18 11:30:19 +0300

    Generics

---
+++ src/main/scala/exercises/MyListCovariant.scala
@@ -0,0 +1,45 @@
+package exercises
+
+abstract class MyList[+A] {
+ /*
+ * head => first element of the list
+ * tail => remainder of the list
+ * isEmpty => is this list empty
+ * add(int) => new list with this element added
+ * toString => a string representation of the list
+ */
+
+ def head: A
+ def tail: MyList[A]
+ def isEmpty: Boolean
+ def add[B >: A](element: B): MyList[B]
+ // polymorphic call
+ def printElements: String
+ override def toString: String = "[" + printElements + "]"
+}
+
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  override def printElements: String = ""
+}
+
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
+  def isEmpty: Boolean = false
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
+  override def printElements: String =
+    if(t.isEmpty) "" + h
+    else h.toString + " " + t.printElements
+}
+
+object ListTest extends App {
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+  
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
+}
---
+++ src/main/scala/lessons/oop/Generics.scala
@@ -0,0 +1,63 @@
+package lessons.oop
+
+object Generics extends App{
+
+//  class MyList[A] {
+//    // use the type A
+//  }
+
+  class MyList[+A] {
+    // use the type A
+    def add[B >: A](element: B): MyList[B] = ???
+    /*
+      A = Cat
+      B = Animal
+    * */
+  }
+
+  class MyMap[Key, Value]
+
+  val listOfIntegers = new MyList[Int]
+  val listOfStrings = new MyList[String]
+
+  // generic methods
+  object MyList {
+    def empty[A]: MyList[A] = ???
+  }
+
+  val emptyListOfIntegers = MyList.empty[Int]
+
+  // variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // 1. yes List[Cat] extends List[Animal] = COVARIANCE
+
+  class CovariantList[+A]
+
+  val animal: Animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  // animalList.add(new Dog)??? HARD QUESTION. => we return a list of Animals
+
+  // 2. NO = INVARIANCE
+  class InvariantList[A]
+
+  val invariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+  // 3 Hell, no! CONTRAVARIANCE
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+  // bounded types
+  // accepts only subtypes of animal
+  class Cage[A <: Animal](animal: A)
+
+  val cage = new Cage(new Dog)
+
+  class Car
+  //val newCage = new Cage(new Car)
+
+  // expand MyList to be generic
+  
+}
